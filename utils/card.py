ace8a81dd5fb3e2e350728fd99622aec2547f4bf
ImaneEF <54639079+ImaneEF@users.noreply.github.com>
2022-02-21 16:31:07 +0100

    "edit card.py file"

--- utils/card.py
+++ utils/card.py
@@ -9,4 +9,4 @@ class Symbol():
 class Card(Symbol):
     def __init__(self, color, icon, value):
         super().__init__(color, icon)
-        self.value
+        self.value = ('A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K')
