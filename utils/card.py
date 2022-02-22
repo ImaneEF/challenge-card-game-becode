5a11d61fe536c5c4340c3261b9f7fbfb727d3bd7
ImaneEF <54639079+ImaneEF@users.noreply.github.com>
2022-02-22 12:32:35 +0100

    "edit card.py & player.py files"

--- utils/card.py
+++ utils/card.py
@@ -1,12 +1,19 @@
-# Make a symbol
 import random
 class Symbol():
-    def __init__(self, color, icon):
+    def __init__(self, color: str, icon):
         self.color = color
-        self.icon = ("♠", "♥", "♦", "♣")
+        self.icon = ["♠", "♥", "♦", "♣" ]
     
 
 class Card(Symbol):
     def __init__(self, color, icon, value):
         super().__init__(color, icon)
-        self.value = ('A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K')
+        self.value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    def print_card(self):
+        print(self.color, self.icon, self.value)
+
+card = Card("red", "♠" , "A")
+card.print_card()
+
+
--- utils/player.py
+++ utils/player.py
@@ -1,3 +1,18 @@
+import sys
+sys.path.append(r'C:\Users\epcs\OneDrive\Documenten\GitHub\challenge-card-game-becode\utils')
+from card import Card
+import random
+from typing import List
 class Player():
-    def __init__(self, cards, turn_count, number_of_cards, history):
-        self.cards = card.py
+    def __init__(self, cards: List[Card], turn_count: int, number_of_cards: int, history):
+        self.cards = [4]
+        self.turn_count = 0
+        self.number_of_cards = 0
+        self.history = history
+
+    def play():
+        random_card = random.choice(Card)
+        print(random_card)
+
+
+
