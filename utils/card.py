973d18fb707ce91896b8d5a494a5480b2ca1a2b5
ImaneEF <54639079+ImaneEF@users.noreply.github.com>
2022-02-23 15:50:31 +0100

    "edit main.py file"

--- utils/card.py
+++ utils/card.py
@@ -2,7 +2,7 @@ import random
 from turtle import color
 
 value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-icon = ["♣", "♢", "♡","♠" ]
+icon = ["♥", "♦", "♣", "♠" ]
 
 class Symbol():
     def __init__(self, icon):
